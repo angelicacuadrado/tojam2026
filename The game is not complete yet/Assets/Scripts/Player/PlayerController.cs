fe4bc94913272cdf24f03350ae5ed9c7cec0683b
Ido Isaac <idoisaac93@gmail.com>
2026-05-08 16:19:41 -0400

    Player and Level1 Basic

--- The game is not complete yet/Assets/Scripts/Player/PlayerController.cs
+++ The game is not complete yet/Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,121 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float groundCheckDistance = 0.1f;
+    private bool isGrounded;
+
+    [Header("Look Settings")]
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform cameraTransform;
+
+    private Rigidbody rb;
+
+    // Input values
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private bool jumpPressed;
+
+    private float xRotation = 0f;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Prevent physics rotation
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleLook();
+        HandleGroundCheck();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    // -----------------------------
+    // INPUT SYSTEM CALLBACKS
+    // -----------------------------
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext ctx)
+    {
+        lookInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+            jumpPressed = true;
+    }
+
+    // -----------------------------
+    // MOVEMENT
+    // -----------------------------
+
+    private void HandleMovement()
+    {
+        Vector3 move = (transform.right * moveInput.x + transform.forward * moveInput.y)
+                       * moveSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + move);
+    }
+
+    // -----------------------------
+    // JUMP
+    // -----------------------------
+
+    private void HandleGroundCheck()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
+    }
+
+    private void HandleJump()
+    {
+        if (jumpPressed && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        jumpPressed = false; // consume jump input
+    }
+
+    // -----------------------------
+    // LOOK
+    // -----------------------------
+
+    private void HandleLook()
+    {
+        float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime;
+
+        // Rotate player horizontally
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Rotate camera vertically
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundCheckDistance);
     }
 }
