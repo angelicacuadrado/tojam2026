85062c6a537e063e0e136d586cebc04bb26bccdd
Ido Isaac <idoisaac93@gmail.com>
2026-05-08 16:26:36 -0400

    PlayerHealth

--- The game is not complete yet/Assets/Scripts/Player/PlayerHealth.cs
+++ The game is not complete yet/Assets/Scripts/Player/PlayerHealth.cs
@@ -3,14 +3,8 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void TakeDamage(int damage, Vector3 direction)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
