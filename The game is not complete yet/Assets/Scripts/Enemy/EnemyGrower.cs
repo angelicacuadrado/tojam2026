c0299978d9c5f13c791d9992bad11a6cd107e36c
Ido Isaac <idoisaac93@gmail.com>
2026-05-08 13:30:07 -0400

    Initial Structure

---
+++ The game is not complete yet/Assets/Scripts/Enemy/EnemyGrower.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyGrower : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
