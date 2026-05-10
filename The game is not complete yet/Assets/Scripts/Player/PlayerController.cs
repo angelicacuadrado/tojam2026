90028e4d4a872f1de0250b0eb3d6def5c595cae9
Maria Angélica Cuadrado Rodriguez <angel.kira65@gmail.com>
2026-05-10 12:08:32 -0400

    Add james audio

--- The game is not complete yet/Assets/Scripts/Player/PlayerController.cs
+++ The game is not complete yet/Assets/Scripts/Player/PlayerController.cs
@@ -92,7 +92,7 @@ public class PlayerController : MonoBehaviour
             {
                 if (NarratorController.Instance != null)
                 {
-                    NarratorController.Instance.PlayLine("Level1_issue1");
+                    NarratorController.Instance.PlayLine("Level1_2");
                 }
                 isWalking = true;
             }
@@ -100,7 +100,10 @@ public class PlayerController : MonoBehaviour
             footstepTimer += Time.deltaTime;
             if (footstepTimer >= footstepInterval)
             {
-                AudioManager.Instance.PlaySFX("PlayerWalk");
+                if (AudioManager.Instance != null)
+                {
+                    AudioManager.Instance.PlaySFX("PlayerWalk");
+                }
                 footstepTimer = 0f;
             }
         }
